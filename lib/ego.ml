666ba511eba51c4fc5253d067680f3b5aa8d5341
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2021-08-31 11:41:43 +0800

    added docs

--- lib/ego.ml
+++
@@ -1,8 +0,0 @@
-module Id = Id
-module Basic = Basic
-module Generic = struct
-  module Query = Query
-  module Scheduler = Scheduler
-  include Language
-  include Generic
-end
--- lib/symbol.mli
+++
@@ -1,8 +0,0 @@
-open Containers
-type t = private int
-val repr : t -> int
-val intern : string -> t
-val pp : Format.formatter -> t -> unit
-val to_string: t -> string
-
-module SymbolMap : Map.S with type key = t
